721fc2bb255c553d2c5a1b11e72cbe61f906fe4d
yuemengrui <1401190335@qq.com>
2023-08-11 09:50:01 +0800

    update

--- ai_server/info/utils/llm_common.py
+++ ai_server/info/utils/llm_common.py
@@ -5,17 +5,18 @@ from info import llm_dict, logger
 from info.modules.protocol import ChatResponse
 
 
-def llm_generate(model_name, prompt, history, history_len=10, stream=True, **kwargs):
-    if stream:
-        for resp in llm_dict[model_name]['model'].lets_chat(prompt, history, stream=True, **kwargs):
-            resp.update({"model_name": model_name})
-            resp["history"] = resp["history"][-history_len:]
-            yield json.dumps(resp, ensure_ascii=False)
-        logger.info(str(resp) + '\n')
-    else:
-        resp = llm_dict[model_name]['model'].lets_chat(prompt, history, stream=False, **kwargs)
-
+def llm_stream_generate(model_name, prompt, history, history_len=10, **kwargs):
+    for resp in llm_dict[model_name]['model'].lets_chat(prompt, history, stream=True, **kwargs):
         resp.update({"model_name": model_name})
         resp["history"] = resp["history"][-history_len:]
-        logger.info(str(resp) + '\n')
-        return ChatResponse(model_name=model_name, **resp)
+        yield json.dumps(resp, ensure_ascii=False)
+    logger.info(str(resp) + '\n')
+
+
+def llm_generate(model_name, prompt, history, history_len=10, **kwargs):
+    resp = llm_dict[model_name]['model'].lets_chat(prompt, history, stream=False, **kwargs)
+
+    resp.update({"model_name": model_name})
+    resp["history"] = resp["history"][-history_len:]
+    logger.info(str(resp) + '\n')
+    return ChatResponse(model_name=model_name, **resp)
